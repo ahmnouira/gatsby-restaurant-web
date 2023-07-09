82d1adb19db953e77e23c27ef9ba925e31447fee
Ahmed Nouria <ahmnouira@gmail.com>
2023-07-09 04:10:42 +0100

    fix: fixed title

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,15 @@
 import * as React from "react"
 import Layout from "../components/layout"
+import { FaBlackTie } from "react-icons/fa"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title={"Home"}></SEO>
-    <h3>The Restaurant Site</h3>
+    <h3>
+      <FaBlackTie />
+      The Restaurant Site
+    </h3>
   </Layout>
 )
 export default IndexPage
