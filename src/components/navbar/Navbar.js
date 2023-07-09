0fc8be3ec9727daf7873c59e46836e6da5ad478d
Ahmed Nouria <ahmnouira@gmail.com>
2023-07-09 05:50:51 +0100

    feat: update navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,14 +1,28 @@
-import React from "react"
+import React, { useState } from "react"
 import NavbarHeader from "./NavbarHeader"
 import NavbarLinks from "./NavbarLinks"
 import NavbarIcons from "./NavbarIcons"
+import { styled } from "styled-components"
 
 export default function Navbar() {
+  const [open, setOpen] = useState(false)
+
+  const handleNavbar = () => {
+    setOpen(value => !value)
+  }
+
   return (
-    <nav>
-      <NavbarHeader />
-      <NavbarLinks />
+    <NavWrapper>
+      <NavbarHeader onClick={handleNavbar} />
+      <NavbarLinks open={open} />
       <NavbarIcons />
-    </nav>
+    </NavWrapper>
   )
 }
+
+const NavWrapper = styled.nav`
+  @media (min-width: 768px) {
+    display: flex;
+    align-items: center;
+  }
+`
