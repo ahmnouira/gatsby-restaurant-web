e351401dd46cdd125683f894cb7cd2afa2972771
Ahmed Nouria <ahmnouira@gmail.com>
2023-07-10 11:51:19 +0100

    feat: added banner

--- src/components/seo.js
+++ src/components/seo.js
@@ -2,19 +2,17 @@ import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 
 function Seo({ description, title, children }) {
-  const { site } = useStaticQuery(
-    graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-            description
-            author
-          }
+  const { site } = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+          description
+          author
         }
       }
-    `
-  )
+    }
+  `)
 
   const metaDescription = description || site.siteMetadata.description
   const defaultTitle = site.siteMetadata?.title
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,18 @@
 import * as React from "react"
 import Layout from "../components/layout"
-import { FaBlackTie } from "react-icons/fa"
-import SEO from "../components/seo"
+import Banner from "../components/banner"
+import Seo from "../components/seo"
 import { HomeHero } from "../components/hero"
 import img from "../images/bcg/homeBcg.jpg"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title={"Home"} />
+    <Seo title={"Home"} />
     <HomeHero img={img}>
-      <h3>
-        <FaBlackTie />
-        The Restaurant Site
-      </h3>
+      <Banner
+        title={"Fine Dinning"}
+        subtitle={"65, MG Road- Bangalore, KA"}
+      ></Banner>
     </HomeHero>
   </Layout>
 )
