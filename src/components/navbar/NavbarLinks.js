5979b3f9294c074e035c75ba5342696366e8ca9f
Ahmed Nouria <ahmnouira@gmail.com>
2023-07-10 07:31:54 +0100

    feat: added navbar links

--- src/components/navbar/NavbarLinks.js
+++ src/components/navbar/NavbarLinks.js
@@ -1,5 +1,79 @@
 import React from "react"
+import { styled } from "styled-components"
+import { styles } from "../../utils"
+import { Link } from "gatsby"
 
-export default function NavbarLinks() {
-  return <div>Component NavbarLinks</div>
+const LINKS = [
+  {
+    id: 0,
+    path: "/",
+    name: "home",
+  },
+  {
+    id: 1,
+    path: "/about/",
+    name: "about",
+  },
+  {
+    id: 2,
+    path: "/menu",
+    name: "menu",
+  },
+  {
+    id: 3,
+    path: "/contact",
+    name: "contact",
+  },
+]
+
+export default function NavbarLinks({ open }) {
+  return (
+    <LinkWrapper open={open}>
+      {LINKS.map(item => {
+        return (
+          <li key={item.id}>
+            <Link to={item.path} className="nav-link">
+              {item.name}
+            </Link>
+          </li>
+        )
+      })}
+    </LinkWrapper>
+  )
 }
+
+const LinkWrapper = styled.ul`
+  li {
+    list-style-type: none;
+  }
+  .nav-link {
+    display: block;
+    text-decoration: none;
+    padding: 0.5rem 1rem 0.5rem 1rem;
+    color: ${styles.colors.mainGrey};
+    font-weight: 700;
+    text-transform: capitalize;
+    cursor: pointer;
+    ${styles.transDefault};
+    &:hover {
+      background: ${styles.colors.mainGrey};
+      color: ${styles.colors.mainYellow};
+      padding: 0.5rem 1rem 0.5rem 1.3rem;
+    }
+  }
+
+  height: ${props => (props.open ? "152px" : "0px")};
+  overflow: hidden;
+  ${styles.transObject({ time: "1s" })}
+
+  // Styles for the Navbar for bigger screens
+  @media (min-width: 768px) {
+    height: auto;
+    display: flex;
+    margin: 0 auto;
+    .nav-link:hover {
+      background: ${styles.colors.mainWhite};
+      padding: 0.5rem 1rem 0.5rem 1rem;
+    }
+  }
+`
--- src/utils/colors.js
+++ src/utils/colors.js
@@ -5,3 +5,13 @@ export const colors = {
   mainYellow2: `#F2AF29`,
   mainGrey: `#474747`,
 }
+
+export const transDefault = "transition: all 0.5s ease-in-out"
+
+export const transObject = ({
+  property = "all",
+  time = "0.5s",
+  type = "ease-in-out",
+}) => {
+  return `transition:${property} ${time} ${type}`
+}
