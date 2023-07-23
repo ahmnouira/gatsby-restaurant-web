4612315a57c1b2919e05f3d5caf0347735f7b109
Ahmed Nouria <ahmnouira@gmail.com>
2023-07-23 21:02:30 +0100

    feat: added our mission

--- src/components/home/QuickInfo.js
+++ src/components/home/QuickInfo.js
@@ -2,12 +2,24 @@ import React from "react"
 import Title from "./Title"
 import styled from "styled-components"
 import { SectionButton } from "../banner/SectionButton"
+import { styles } from "../../utils"
+import { Link } from "gatsby"
 
 export default function QuickInfo() {
   return (
     <Section>
       <Title title={"our mission"} message={"let us tell you"}></Title>
-      <SectionButton style={{ margin: "2rem auto" }}>about</SectionButton>
+      <QuickInfoWrapper>
+        <p className="text">
+          Our mission is to serve you authetic dishes from north karnataka.
+          these dishes are created by our top chefs, from the region. Some of
+          our awesome dishes are - Menther kadabu, Pundi Soppu, Jolad Rotti,
+          Ragi Ambali, Nargis Man Dak ki, Mudde, Girmit and many more.
+        </p>
+        <Link to="/about/" style={{ textDecoration: "none" }}>
+          <SectionButton style={{ margin: "2rem auto" }}>about</SectionButton>
+        </Link>
+      </QuickInfoWrapper>
     </Section>
   )
 }
@@ -16,4 +28,22 @@ const Section = styled.section`
   padding: 2rem 0;
   width: 90vw;
   margin: 0 auto;
+`
+
+const QuickInfoWrapper = styled.div`
+  width: 90%;
+  margin: 2rem auto;
+  .text {
+    line-height: 2rem;
+    color: ${styles.colors.mainGrey};
+    word-spacing: 0.2rem;
+  }
+
+  @media (min-width: 768px) {
+    width: 70%;
+  }
+
+  @media (min-width: 992px) {
+    width: 60%;
+  }
 `
